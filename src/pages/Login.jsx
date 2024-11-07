d0e45e59ef64a644c5647caedfe783fb934c2bdb
Pentapati Seshagiri <Pentapati.Seshagiri@ivycomptech.com>
2024-11-07 18:26:54 +0530

    added register and login page layout

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,29 @@
-
-import { Link } from "react-router-dom";
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { FormRow, Logo } from '../components';
+import { Link, Form } from "react-router-dom";
 const Login = () => {
   return (
-    <div>
-   <h1>Login Page</h1>
-  <Link to='/register'>Register Page</Link>
-  </div>
+    <Wrapper>
+    <Form method='post' className='form'>
+      <Logo />
+      <h4>login</h4>
+      <FormRow type='email' name='email' />
+      <FormRow type='password' name='password' />
+      {/* <SubmitBtn /> */}
+      <button type='button' className='btn btn-block'>
+       login
+      </button>
+      <button type='button' className='btn btn-block'>
+        explore the app
+      </button>
+      <p>
+        Not a member yet?
+        <Link to='/register' className='member-btn'>
+          Register
+        </Link>
+      </p>
+    </Form>
+  </Wrapper>
   )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,30 @@
 
-import { Link } from "react-router-dom";
+import { Link, Form } from "react-router-dom";
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { FormRow,Logo } from "../components";
 const Register = () => {
   return (
-    <div>
-    <h1>Register Page</h1>;
-    <Link to='/login'>Login Page</Link>
-    </div>
+    <Wrapper>
+    <Form method='post' className='form'>
+      <Logo />
+      <h4>Register</h4>
+      <FormRow type='text' name='name' />
+      <FormRow type='text' name='lastName' labelText='last name' />
+      <FormRow type='text' name='location' />
+      <FormRow type='email' name='email' />
+      <FormRow type='password' name='password' />
+      {/* <SubmitBtn /> */}
+      <button type='submit' className="btn btn-block">
+        submit
+      </button>
+      <p>
+        Already a member?
+        <Link to='/login' className='member-btn'>
+          Login
+        </Link>
+      </p>
+    </Form>
+  </Wrapper>
   )
 }
 
